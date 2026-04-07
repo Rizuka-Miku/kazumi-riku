97a7078b01706bdfbd838535be15c5a4facc0f8b
Moh Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2026-04-07 21:08:39 +0700

    fix linter

--- constants/hangmanWords.js
+++ constants/hangmanWords.js
@@ -1,103 +1,103 @@
 export const songs = [
-    {
-        "title": "Palu Keberanian",
-        "hint": ['lagunya diawali dengan kick dan clap', 'Isshounya isshou penambang', 'M01 dari setlist Team J']
-    },
-    {
-        "title": "Di Bawah Langit Berwarna Sakura",
-        "hint": ['Salah satu lagu dalam album mahagita', 'Pernah dibawakan oleh JKT48 Accoustic', 'Pernah dibawakan oleh Sheila & The upmost ft JKT48 Accoustic']
-    },
-    {
-        "title": "Sweet and Bitter",
-        "hint": ['Merupakan salah satu coupling song dari single JKT48','MV Bertemakan salju', 'Coupling song rapsodi']
-    },
-    {
-        "title": "Balada Serangga",
-        "hint": ['Lagu ini dinyanyikan oleh 1 orang', 'Beby pernah membawakan lagu ini', 'Lagu ini Identik dengan Sisca']
-    },
-    {
-        "title": "Cara Ceroboh Untuk Mencinta",
-        "hint": ['Dibawakan oleh senbatsu management', 'Merupakan coupling song #Sukinanda (di AKB)', 'single yang muncul saat pandemi']
-    },
-    {
-        "title": "Aku Sangat Suka",
-        "hint": ['Merupakan Lagu sebelum encore', 'Lagu ini merupakan lagu dari setlist yang aktif saat ini']
-    },
-    {
-        "title": "Pergi Untuk Bertemu",
-        "hint": ['Merupakan lagu dari setlist team KIII', 'Merupakan lagu sebelum encore', 'Pernah dibawakan di Nadila Special Graduation Concert']
-    },
-    {
-        "title": "Bersama Kamu Pelangi dan Mentari",
-        "hint": ['Lagu yang berada di dalam album Joy Kick Tears', 'Lagu ini mempunyai lyric video']
-    },
-    {
-        "title": "Relax",
-        "hint": ['Lagu ini dibawakan oleh 3 orang', 'Lagu ini merupakan lagu benefit dari top 3 sousenkyou', 'Lagu ini dibawakan dalam setlist Fly! Team T']
-    },
-    {
-        "title": "Ku tak Pantas Jadi Seorang Pujangga Cinta",
-        "hint": ['Lagu ini merupakan M01', 'Lagu ini masuk kedalam Request Hour 2021', 'Lagu ini identik dengan Fresh Peach']
-    },
-    {
-        "title": "Schoolyard Puppy",
-        "hint": ['Lagu ini merupakan lagu dari setlist aktif saat ini', 'Lagu ini mempunyai 1 peran yang selalu berganti ganti tiap shownya', 'Peran yang selalu berganti ini memakai telinga anjing']
-    }, 
-    {
-        "title": "Tidak boleh pelukan", 
-        "hint": ['Merupakan lagu coupling', 'Sinka menjadi center di lagu ini', 'lagu coupling dari mae shika mukane']
-    },
-    {
-        "title": "Dibanding Kemarin Semakin Suka",
-        "hint": ['Merupakan lagu di dalam album Pareo wa emerald', 'Lagu ini dibawakan pada event ramadhan kemarin']
-    },
-    {
-        "title": "Pertahanan akan cinta",
-        "hint": ['lagu dari setlist boku no taiyou', 'Lagu ini dibawakan oleh 3 orang']
-    }, 
-    {
-        "title": "Mawar Natal Musim Panas",
-        "hint": ['Lagu ini dibawakan 4 orang', 'Merupakan lagu dalam setlist RKJ']
-    },
-    {
+	{
+		'title': 'Palu Keberanian',
+		'hint': ['lagunya diawali dengan kick dan clap', 'Isshounya isshou penambang', 'M01 dari setlist Team J'],
+	},
+	{
+		'title': 'Di Bawah Langit Berwarna Sakura',
+		'hint': ['Salah satu lagu dalam album mahagita', 'Pernah dibawakan oleh JKT48 Accoustic', 'Pernah dibawakan oleh Sheila & The upmost ft JKT48 Accoustic'],
+	},
+	{
+		'title': 'Sweet and Bitter',
+		'hint': ['Merupakan salah satu coupling song dari single JKT48', 'MV Bertemakan salju', 'Coupling song rapsodi'],
+	},
+	{
+		'title': 'Balada Serangga',
+		'hint': ['Lagu ini dinyanyikan oleh 1 orang', 'Beby pernah membawakan lagu ini', 'Lagu ini Identik dengan Sisca'],
+	},
+	{
+		'title': 'Cara Ceroboh Untuk Mencinta',
+		'hint': ['Dibawakan oleh senbatsu management', 'Merupakan coupling song #Sukinanda (di AKB)', 'single yang muncul saat pandemi'],
+	},
+	{
+		'title': 'Aku Sangat Suka',
+		'hint': ['Merupakan Lagu sebelum encore', 'Lagu ini merupakan lagu dari setlist yang aktif saat ini'],
+	},
+	{
+		'title': 'Pergi Untuk Bertemu',
+		'hint': ['Merupakan lagu dari setlist team KIII', 'Merupakan lagu sebelum encore', 'Pernah dibawakan di Nadila Special Graduation Concert'],
+	},
+	{
+		'title': 'Bersama Kamu Pelangi dan Mentari',
+		'hint': ['Lagu yang berada di dalam album Joy Kick Tears', 'Lagu ini mempunyai lyric video'],
+	},
+	{
+		'title': 'Relax',
+		'hint': ['Lagu ini dibawakan oleh 3 orang', 'Lagu ini merupakan lagu benefit dari top 3 sousenkyou', 'Lagu ini dibawakan dalam setlist Fly! Team T'],
+	},
+	{
+		'title': 'Ku tak Pantas Jadi Seorang Pujangga Cinta',
+		'hint': ['Lagu ini merupakan M01', 'Lagu ini masuk kedalam Request Hour 2021', 'Lagu ini identik dengan Fresh Peach'],
+	},
+	{
+		'title': 'Schoolyard Puppy',
+		'hint': ['Lagu ini merupakan lagu dari setlist aktif saat ini', 'Lagu ini mempunyai 1 peran yang selalu berganti ganti tiap shownya', 'Peran yang selalu berganti ini memakai telinga anjing'],
+	},
+	{
+		'title': 'Tidak boleh pelukan',
+		'hint': ['Merupakan lagu coupling', 'Sinka menjadi center di lagu ini', 'lagu coupling dari mae shika mukane'],
+	},
+	{
+		'title': 'Dibanding Kemarin Semakin Suka',
+		'hint': ['Merupakan lagu di dalam album Pareo wa emerald', 'Lagu ini dibawakan pada event ramadhan kemarin'],
+	},
+	{
+		'title': 'Pertahanan akan cinta',
+		'hint': ['lagu dari setlist boku no taiyou', 'Lagu ini dibawakan oleh 3 orang'],
+	},
+	{
+		'title': 'Mawar Natal Musim Panas',
+		'hint': ['Lagu ini dibawakan 4 orang', 'Merupakan lagu dalam setlist RKJ'],
+	},
+	{
 
-        "title": "Musim Panas Sounds Good",
-        "hint": ['Merupakan Single JKT48', 'Lagu ini MV nya bertemakan pantai', 'Lagu ini mempunyai versi bahasa inggris']
-    }, 
-    {
-        "title": "Joan of Arc di dalam Cermin",
-        "hint": ['Dibawakan oleh 5 orang', 'Center lagu ini membawa bendera', 'Kinal menjadi center di lagu ini']
-    },
-    {
-        "title": "Pelaut yang Melihat Mimpi di tengah Badai",
-        "hint": ['Merupakan lagu encore', 'Isshou lagu ini adalah kaos jeketi dengan celana pendek']
-    },
-    {
-        "title": "New Ship",
-        "hint": ['Merupakan lagu dalam album mahagita 2', 'Mempunyai isshou lucu seperti pelaut', 'Melody ada di MV lagu ini versi AKB48']
-    },
-    {
-        "title": "Kaca berbentuk I Love You",
-        "hint": ['merupakan lagu dari setlist aktif', 'lagu ini dibawakan oleh 4 orang', 'lagu ini merupakan lagu dari setlist Fly, Team T!']
-    },
-    {
-        "title": "After Rain",
-        "hint": ["Lagu ini mempunyai lyric video", "Lagu ini mempunyai chant video tutorial", "Lagu ini baru - baru ini viral"]
-    },
-    {
-        "title": "So Long",
-        "hint": ["Lagu ini pernah dibawakan dalam setlist banzai", 'MV lagu ini bertemakan musim dingin', 'Lagu ini merupakan lagu graduation Jessica Veranda']
-    },
-    {
-        "title": "Jika Aku Dipelukmu",
-        "hint": ['Lagu ini dibawakan oleh 3 orang', 'Lagu ini pernah dibawakan 6 orang pada konser KIII VS KIII', 'Lagu ini memakai kursi sebagai properti']
-    },
-    {
-        "title": "To be Continued",
-        "hint": ['Lagu ini merupakan lagu terakhir dari sebuah setlist', 'Lagu ini merupakan lagu dalam setlist saka agari']
-    },
-    {
-        "title": "Aishiteraburu",
-        "hint": ["Lagu ini berada di album suzukake nancara"]
-    }
+		'title': 'Musim Panas Sounds Good',
+		'hint': ['Merupakan Single JKT48', 'Lagu ini MV nya bertemakan pantai', 'Lagu ini mempunyai versi bahasa inggris'],
+	},
+	{
+		'title': 'Joan of Arc di dalam Cermin',
+		'hint': ['Dibawakan oleh 5 orang', 'Center lagu ini membawa bendera', 'Kinal menjadi center di lagu ini'],
+	},
+	{
+		'title': 'Pelaut yang Melihat Mimpi di tengah Badai',
+		'hint': ['Merupakan lagu encore', 'Isshou lagu ini adalah kaos jeketi dengan celana pendek'],
+	},
+	{
+		'title': 'New Ship',
+		'hint': ['Merupakan lagu dalam album mahagita 2', 'Mempunyai isshou lucu seperti pelaut', 'Melody ada di MV lagu ini versi AKB48'],
+	},
+	{
+		'title': 'Kaca berbentuk I Love You',
+		'hint': ['merupakan lagu dari setlist aktif', 'lagu ini dibawakan oleh 4 orang', 'lagu ini merupakan lagu dari setlist Fly, Team T!'],
+	},
+	{
+		'title': 'After Rain',
+		'hint': ['Lagu ini mempunyai lyric video', 'Lagu ini mempunyai chant video tutorial', 'Lagu ini baru - baru ini viral'],
+	},
+	{
+		'title': 'So Long',
+		'hint': ['Lagu ini pernah dibawakan dalam setlist banzai', 'MV lagu ini bertemakan musim dingin', 'Lagu ini merupakan lagu graduation Jessica Veranda'],
+	},
+	{
+		'title': 'Jika Aku Dipelukmu',
+		'hint': ['Lagu ini dibawakan oleh 3 orang', 'Lagu ini pernah dibawakan 6 orang pada konser KIII VS KIII', 'Lagu ini memakai kursi sebagai properti'],
+	},
+	{
+		'title': 'To be Continued',
+		'hint': ['Lagu ini merupakan lagu terakhir dari sebuah setlist', 'Lagu ini merupakan lagu dalam setlist saka agari'],
+	},
+	{
+		'title': 'Aishiteraburu',
+		'hint': ['Lagu ini berada di album suzukake nancara'],
+	},
 ];
